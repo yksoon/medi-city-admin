8ab437ddfc57a24a068ca51a48b02870ecc8bde7
Eunji <ej.lim@hicomp.net>
2023-07-05 18:33:48 +0900

    배포가 왜 안될까....................

--- publish/js/basic.js
+++
@@ -1,8 +0,0 @@
-// modal
-function modal_close(){
-    document.getElementById('modal_wrap').style.display = "none";
-}
-function modal_open(content){
-    document.getElementById('modal_wrap').style.display = "block";
-    let modal = document.getElementById('modal');
-}
