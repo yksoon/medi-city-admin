bc9f294a0cd4674e9c2d52e19b4e0de970a3f803
Eunji <ej.lim@hicomp.net>
2023-07-14 11:13:22 +0900

    230714

--- publish/js/basic.js
+++ publish/js/basic.js
@@ -2,7 +2,18 @@
 function modal_close(){
     document.getElementById('modal_wrap').style.display = "none";
 }
-function modal_open(content){
-    document.getElementById('modal_wrap').style.display = "block";
-    let modal = document.getElementById('modal');
-}
+
+//document.querySelectorAll('.modal_content').style.display = "none";
+//
+//function modal_open(content){
+//    console.log(content);
+//    document.getElementById('modal_wrap').style.display = "block";
+//    const modalContent = content;
+//    modalContent.classList.add('block');
+//}
+
+$('.modal_btn').click(function(){
+    $('#modal_wrap').show();
+    $('.modal_content').hide();
+    $($(this).attr('title')).show();
+})
